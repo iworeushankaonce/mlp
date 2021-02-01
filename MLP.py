20cc7c3f2be2e26b9abfe6d2930abcef8e994d13
iworeushankaonce <gabdulkhakovarslan@gmail.com>
2021-02-02 00:26:40 +0100

    Updated Readme and MLP.py

--- MLP.py
+++ MLP.py
@@ -12,10 +12,11 @@ class MLP():
     | initialization:        {'normal','xavier_normal','xavier_uniform'} 
     |                        weight initialization technique
     |                        'normal' by default
+    | activation:            
     '''
     
     def __init__(self, input_dim=2, hidden_layer_units=10, eta=0.005,
-                 initialization='normal'):
+                 initialization='normal',hidden_activation='tanh'):
         # input dimensions
         self.input_dim = input_dim
         # number of units in the hidden layer
@@ -30,23 +31,34 @@ class MLP():
           self.weight_init_xavier_normal()    
         elif initialization == 'xavier_uniform':
           self.weight_init_xavier_uniform()
+        # set proper activation function
+        self.a=list()
+        self.a_d=list()
+        if hidden_activation=='tanh':
+          self.a.append(self.tanh)
+          self.a_d.append(self.tanh_derivative)
+        elif hidden_activation=='relu':
+          self.a.append(self.relu)
+          self.a_d.append(self.relu_derivative)
+        
+        self.a.append(self.tanh)
+        self.a_d.append(self.tanh_derivative)             
 
     def tanh(self,a):
       return np.tanh(a)
-
     def tanh_derivative(self,a):
       return (1 - (self.tanh(a)**2))
 
-    def relu(self, a):
-      return (np.max(0,a))
+    def relu(self,a):
+      return np.clip(a, a_min=0, a_max=None)
     def relu_derivative(self,a):
-      return 0 if a <= 0 else 1  
+      return  a > 0
+     
     
     def l2_loss(self,a,b):
       return np.square(a-b).mean()
-
-    def a(self,w,h):
-      return np.matmul(w,h)
+    def l2_loss_d(self):
+      pass  
 
     def weight_init_default(self):
       self.weights.append(np.random.normal(0, 1, (self.hidden_layer_units, self.input_dim+1)))
@@ -68,20 +80,32 @@ class MLP():
             
 
 
-    def predict(self,x,y):
+    def predict(self,x,y,internal=False):
       bias = np.ones((1))
       X_b = np.concatenate((bias,x))
       x = np.reshape(X_b, (3,1))
       #forward pass
         
-      a_l = self.a(self.weights[0], x)
-      h_l = self.tanh(a_l); h_l = np.concatenate((np.ones((1,1)), h_l))
+      a_l = np.matmul(self.weights[0], x)
+      h_l = self.a[0](a_l); h_l = np.concatenate((np.ones((1,1)), h_l))
 
-      a_m = self.a(self.weights[1],h_l)
-      return self.tanh(a_m) 
+      a_m = np.matmul(self.weights[1],h_l)
+      h_m = self.a[-1](a_m) 
+      if internal:
+        return (x,a_l,h_l,a_m,h_m)
+      else:
+        return h_m
 
     
     def evaluate(self,X,y):
+      '''
+      This method computes the mean squared error (MSE) 
+      on the test set and returns the list of misclassified 
+      points (using 0 as the classification threshold).
+      | **Args**
+      | X:                  Evaluation examples.
+      | y:                  Ground truth labels.
+      '''
       mse=list()
       misclassified_points=list()
       n_misclassified = 0
@@ -92,7 +116,7 @@ class MLP():
         if(y_pred>=0 and y[i]==1) or (y_pred<0 and y[i]==-1):
           pass
         else:
-          misclassified_points.append((i,el))   
+          misclassified_points.append((i,el[0],el[1]))   
       return np.mean(mse),misclassified_points
     
     def train(self, X, y, epochs=100):
@@ -110,35 +134,26 @@ class MLP():
         # 2. start with output layer, and traverse back by 5d
 
         loss=list()
-        bias = np.ones(len(X))
-        X_b = np.vstack((bias,X.T)).T
         
-        shuffle_indxs = np.random.permutation(len(X))
-        X_b = X_b[shuffle_indxs]
-        y = y[shuffle_indxs]
         for e in range(epochs):
+          shuffle_indxs = np.random.permutation(len(X))
+          X = X[shuffle_indxs]
+          y = y[shuffle_indxs]
           #if (e+1)%10==0:
           print("Epoch ",e+1)
           for i in range(len(y)):
-            x = np.reshape(X_b[i], (3,1))
-            
             #forward pass
+            (x,a_l,h_l,a_m,h_m) = self.predict(X[i],y[i],internal=True)
             
-            a_l = self.a(self.weights[0], x)
-            h_l = self.relu(a_l); h_l = np.concatenate((np.ones((1,1)), h_l))
-
-            a_m = self.a(self.weights[1],h_l)
-            h_m = y_pred = self.tanh(a_m) 
-            
-            error = self.l2_loss(y[i],y_pred)
+            error = (h_m-y[i])**2
             loss.append(error)
             #backward pass
 
-            err_m = (self.tanh_derivative(a_m)) * 2*(y_pred-y[i])
+            err_m = (self.a_d[-1](a_m)) * 2*(h_m-y[i])
             
-            # the bias has to be removed from his function, thus
-            # self.weights[1][:,1:].T
-            err_l = (self.tanh_derivative(a_l)) * np.matmul(self.weights[1][:,1:].T, err_m)
+            # the bias has to be removed from his function,
+            # thus self.weights[1][:,1:].T
+            err_l = (self.a_d[0](a_l)) * np.matmul(self.weights[1][:,1:].T, err_m)
 
             self.weights[1] -= self.eta*(np.outer(err_m, h_m))
             self.weights[0] -= self.eta*(np.outer(err_l, x)) 
@@ -147,7 +162,7 @@ class MLP():
 
 if __name__ == "__main__":
     '''
-    Train MLPs as defined in the assignments.
+    Prepare dataset and train MLP 
     '''
     # load dataset
     dataset = np.load('xor.npy')
